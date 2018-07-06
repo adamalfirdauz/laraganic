1da64f89f3475a9e897839c85cc365567dc71b2d
octavianwr <octavian_wr@yahoo.com>
2018-07-06 20:42:51 +0700

    edit interface

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -24,7 +24,7 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                
             </div>
         </div>
         <!-- ./col -->
@@ -39,7 +39,7 @@
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                
             </div>
         </div>
         <!-- ./col -->
@@ -54,7 +54,7 @@
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                
             </div>
         </div>
         <!-- ./col -->
@@ -69,7 +69,7 @@
                 <div class="icon">
                     <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                
             </div>
         </div>
         <!-- ./col -->
